0c45bd2f256a2b373e7fc281cc6daf32b123547e
Trend20 <enockomondi305@gmail.com>
2022-07-23 23:21:06 +0300

    course component

--- client/src/components/Courses/Courses.js
+++ client/src/components/Courses/Courses.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 const Courses = () =>{
+
+  const [showContent, setShowContent] = useState(false);
+
+  const showList = () =>{
+    setShowContent(true)
+  }
   return (
     <CoursesContainer>
       <Heading>Free Courses</Heading>
       <CourseCategoryWrapper>
-        <Category></Category>
+        <Category>
+          <h3>Web Development</h3>
+        </Category>
+        <Category>
+        <h3>Digital Marketing</h3>
+        </Category>
+        <Category>
+        <h3>Web Design</h3>
+        </Category>
+        <Category>
+        <h3>Ethical Hacking</h3>
+        </Category>
       </CourseCategoryWrapper>
     </CoursesContainer>
   )
@@ -14,7 +31,14 @@ const Courses = () =>{
 
 const CoursesContainer = styled.div`
   display: flex;
-  width: 100%;
+  width: 80%;
+  margin: auto;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+   margin-top: 30px;
+   padding-bottom: 50px;
 `
 
 const Heading = styled.h3`
@@ -22,12 +46,17 @@ const Heading = styled.h3`
 `
 
 const CourseCategoryWrapper = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(2, 2fr);
+  grid-gap: 20px;
   width: 100%;
 `
 
 const Category = styled.div`
   display: flex;
+  flex-direction: column;
+  background-color: #0bce97;
+  color: #fff;
 `
 
 export default Courses
